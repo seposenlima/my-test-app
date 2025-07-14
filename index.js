e75798899e72a0a03760bb3c05b3ce6fd7364d39
root <root@ip-172-31-28-46.ap-southeast-1.compute.internal>
2025-07-14 14:59:16 +0000

    Update message for staging test

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const http = require('http');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'myuser',
+  password: 'Amalmusa@01',
+  database: 'myapp'
+});
+
+// Connect to MySQL
+db.connect(err => {
+  if (err) {
+    console.error('❌ MySQL connection failed:', err);
+    process.exit(1);
+  }
+  console.log('✅ Connected to MySQL');
+});
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' }); 
-  res.end("🚀 Hello world from GitHub Actions Live Deployment!");
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+
+  db.query('SELECT NOW() AS time', (err, results) => {
+    if (err) {
+      res.end('Database error');
+      return;
+    }
+
+    res.end(`🚀 Hello! DB time: ${results[0].time}`);
+  });
 });
 
 const PORT = 3000;
